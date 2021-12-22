683197603c16b1fccc9534f9019dc39240d40485
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2021-12-22 12:19:25 -0500

    Depart and Return Calendar buttons fixed

--- FlightStation/app/src/main/java/winter2021_2022/flightstation/MainPage.java
+++ FlightStation/app/src/main/java/winter2021_2022/flightstation/MainPage.java
@@ -17,8 +17,16 @@ import java.util.Calendar;
 public class MainPage extends AppCompatActivity {
     private Button login;
 
-    TextView tvDate;
-    DatePickerDialog.OnDateSetListener setListener;
+    /* Used for departure and return dates */
+    Calendar calendar = Calendar.getInstance();
+    final int year = calendar.get(Calendar.YEAR);
+    final int month = calendar.get(Calendar.MONTH);
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    TextView departDate;
+    DatePickerDialog.OnDateSetListener setListenerDepart;
+    TextView returnDate;
+    DatePickerDialog.OnDateSetListener setListenerReturn;
 
 
     @Override
@@ -26,34 +34,52 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
 
-        /*------------------------------Scrolling Calendar Selection---------------------------*/
-        tvDate = findViewById(R.id.tv_date);
-        Calendar calendar = Calendar.getInstance();
-        final int year = calendar.get(Calendar.YEAR);
-        final int month = calendar.get(Calendar.MONTH);
-        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        /*------------------------------Scrolling Calendar Selection - Departure ---------------------------*/
+        departDate = findViewById(R.id.inputDepartDate);
 
-        tvDate.setOnClickListener(new View.OnClickListener() {
+        departDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
                         MainPage.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth
-                        , setListener, year, month, day);
+                        , setListenerDepart, year, month, day);
                 datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 datePickerDialog.show();
             }
         });
 
-        setListener = new DatePickerDialog.OnDateSetListener() {
+        setListenerDepart = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
                 month += 1;
                 String date = day+"/"+month+"/"+year;
-                tvDate.setText(date);
+                departDate.setText(date);
+            }
+        };
+
+        /*------------------------------Scrolling Calendar Selection - Return---------------------------*/
+        returnDate = findViewById(R.id.inputReturnDate);
+
+        returnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainPage.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        , setListenerReturn, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListenerReturn = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month += 1;
+                String date = day+"/"+month+"/"+year;
+                returnDate.setText(date);
             }
         };
         /*----------------------------------------------------------------------------------------*/
-
 
         // changing from loginMainPage -> loginPage
         this.login = findViewById(R.id.buttonLoginMainPage);
