d9d66f119d2ec0136fe1163ebffee16663c8b123
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2021-12-22 01:21:49 -0500

    Scrolling Calendar Added

--- FlightStation/app/src/main/java/winter2021_2022/flightstation/MainPage.java
+++ FlightStation/app/src/main/java/winter2021_2022/flightstation/MainPage.java
@@ -2,19 +2,60 @@ package winter2021_2022.flightstation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainPage extends AppCompatActivity {
     private Button login;
+
+    TextView tvDate;
+    DatePickerDialog.OnDateSetListener setListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
 
+        /*------------------------------Scrolling Calendar Selection---------------------------*/
+        tvDate = findViewById(R.id.tv_date);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainPage.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        , setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month += 1;
+                String date = day+"/"+month+"/"+year;
+                tvDate.setText(date);
+            }
+        };
+        /*----------------------------------------------------------------------------------------*/
+
+
+        // changing from loginMainPage -> loginPage
         this.login = findViewById(R.id.buttonLoginMainPage);
         this.login.setOnClickListener(new View.OnClickListener() {
             @Override
